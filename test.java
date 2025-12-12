de4d602463aba54ad0379f226132dacdf40c4f07
Abanoup Nasif <bebo.6ed@gmail.com>
2025-12-12 23:44:06 +0200

    rem

--- test.java
+++
@@ -1,7 +0,0 @@
-@Test
-public void testCalculateGrade() {
-    Student student = new Student("Ahmed", 82.0);
-    String grade = student.calculateGrade();
-
-    assertEquals("B", grade, "Grade should be B");
-}
